88bb9a99c81626c14b4e803e038c22c495c68aae
deepak <deepak.singh@thethingscloud.com>
2019-12-18 10:47:58 +0530

    changes

--- Linked Lists/linked_list.c
+++ Linked Lists/linked_list.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct node 
-{
+typedef struct node {
     int data;
     struct node *next;
 } node;
-
 
 void append(node**, int);
 void add_at_beg(node**, int);
@@ -14,8 +12,7 @@ void add_after(node*, int, int);
 void deleteNode(node **h, int value);
 
 
-int main()
-{
+int main() {
     node *head;
     head = NULL;
     
@@ -56,84 +53,61 @@ int main()
 
 
 // inserts a new node at the end of the linked list
-void append(node **h, int value)
-{
+void append(node **h, int value) {
     node *temp, *current;
-    
     temp = malloc(sizeof(node));
     temp->data = value;
     temp->next = NULL;
-    
-    if(*h == NULL)     // if the list(or head) is empty
-    {
+    if(*h == NULL) {     // if the list(or head) is empty
         *h = temp;     // makes temp as the head of the list
     }
-    else
-    {
-        current = *h;
-          
+    else {
+        current = *h; 
         while(current->next != NULL)     // Traverse to the last node of the list
-            current = current->next; 
-            
+            current = current->next;            
         current->next = temp;     // the link part of last node points to temp
     }
 } 
 
 
 // inserts a new node at the beginning of the linked list.
-void add_at_beg(node **h, int value)
-{
+void add_at_beg(node **h, int value) {
     node *temp;
-    
     temp = malloc(sizeof(node));
     temp->data = value;
     temp->next = *h;     // the link part of temp points to the head of the list
-    
     *h = temp;     // makes temp as the head of the list
 }
 
 
 // Inserts after given no. of nodes
-void add_after(node *h, int n, int value)
-{
+void add_after(node *h, int n, int value) {
     node *temp, *current;
     int i;
-    
     temp = (node*)malloc(sizeof(node));
     temp->data = value;
-    
     current = h;
-    
     for(i=1; i<n; i++)     // Traverse to the nth node of the list
-    {
         current = current->next;
-    }
-    
     temp->next = current->next;     // the link part of temp points to the (n+1)th node of the list
     current->next = temp;     // the link part of nth node points to temp
 }
 
 
 // deletes all the nodes having the given value
-void deleteNode(node **h, int value)
-{
+void deleteNode(node **h, int value) {
     node *current, *temp;
     current = *h;
-    
-    while(current != NULL)
-    {
+    while(current != NULL) {
         // if the node to be deleted is the first node of the list
-        if(current == *h && current->data == value)    
-        {
+        if(current == *h && current->data == value) {
             temp = *h;
             *h = current->next;     // the next node becomes head
             free(temp);
-            
             current = *h;
         }
         // if the node to be deleted is other than first node
-        else if(current->next != NULL && current->next->data == value)
-        {
+        else if(current->next != NULL && current->next->data == value) {
             temp = current->next;     // this node is to be deleted
             current->next = temp->next;
             free(temp);
@@ -145,12 +119,9 @@ void deleteNode(node **h, int value)
 
 
 // for displaying the linked list
-display(node *q)
-{
+display(node *q) {
     printf("\n");
-    
-    while(q != NULL)
-    {
+    while(q != NULL) {
         printf("%d => ", q->data);
         q = q->next;
     }
@@ -159,12 +130,9 @@ display(node *q)
 
 
 // counts the no. of nodes in the list
-count(node *q)
-{
+count(node *q) {
     int c = 0;
-    
-    while(q != NULL)
-    {
+    while(q != NULL) {
         q = q->next;
         c++;
     }
